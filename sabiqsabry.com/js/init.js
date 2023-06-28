89c060f3098fcdd9bd9d740f20becfbf5803e31b
Sabiq Sabry <sabiqsabry48@gmail.com>
2023-06-28 05:19:30 +0100

    added the blog sections and updated certain project areas

--- sabiqsabry.com/js/init.js
+++ sabiqsabry.com/js/init.js
@@ -512,7 +512,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Freelancer", "Web Developer", "Full Stack Developer", "iOS Developer", "Technology Expert"],
+			strings: ["Freelance Developer", "Full Stack Developer", "iOS Developer", "Technology Expert"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
